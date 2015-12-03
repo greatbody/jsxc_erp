0e08e970f207524fb6f704babeaa0a0e79df002f
greatbody <wedown@163.com>
2015-12-03 17:09:40 +0800

    初步初始化devise for User

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151203090511) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "phone",               limit: 255, default: "", null: false
+    t.string   "email",               limit: 255, default: "", null: false
+    t.string   "encrypted_password",  limit: 255, default: "", null: false
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",       limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",  limit: 255
+    t.string   "last_sign_in_ip",     limit: 255
+    t.datetime "created_at",                                   null: false
+    t.datetime "updated_at",                                   null: false
+  end
+
+  add_index "users", ["phone"], name: "index_users_on_phone", unique: true, using: :btree
 
 end
