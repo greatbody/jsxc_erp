5df9b4677c9d353e3f4c26fc21bff765fcf58d4e
greatbody <wedown@163.com>
2015-12-07 14:07:27 +0800

    add current_status enum method for select

--- app/models/contact_log.rb
+++ app/models/contact_log.rb
@@ -3,11 +3,17 @@ class ContactLog < ActiveRecord::Base
   belongs_to :user
 
   enum contact_type: [:intent, :trylearn, :sign_up, :suggestion, :complaint]
-  enum current_status: [:wait_call, :in_contact, :booked_try, :wait_pay, :signed_up]
+  enum current_status: [:wait_call, :contacting, :booking, :wait_pay, :signed_up]
 
   def self.contact_types_for_select
     contact_types.map do |contact_type, _|
       [I18n.t("contact_types.#{contact_type}"), contact_type]
+    end
+  end
+
+  def self.current_status_for_select
+    current_statuses.map do |current_status, _|
+      [I18n.t("current_status.#{current_status}"), current_status]
     end
   end
 
