c0bfc8406e364d8fd7c5d563878444c0b8ec1e37
greatbody <wedown@163.com>
2015-12-08 23:36:34 +0800

    change deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-set :rbenv_ruby, '2.2.3'
+set :rbenv_ruby, '2.2.2'
 
 namespace :deploy do
 
