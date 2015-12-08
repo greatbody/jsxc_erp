32336a3e127abece91bb808a6bfe3b4808c4c39b
greatbody <wedown@163.com>
2015-12-08 15:26:50 +0800

    add method to get last next_contact_at

--- app/models/intention.rb
+++ app/models/intention.rb
@@ -1,4 +1,13 @@
 class Intention < ActiveRecord::Base
   belongs_to :student
   belongs_to :user
+
+  def next_contact_date
+    last_contact = ContactLog.where(student_id: student.id).order(created_at: :desc).first
+    if last_contact.attribute_present?(:next_contact_at) then
+      last_contact.next_contact_at.to_date
+    else
+      nil
+    end
+  end
 end
