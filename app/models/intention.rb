5b8ef73692dab1abd6f724e5920d7242ce902e3a
greatbody <wedown@163.com>
2015-12-08 22:42:04 +0800

    1.add current_status to Intentions enum 2.fix error when intention has no contact_log

--- app/models/intention.rb
+++ app/models/intention.rb
@@ -2,8 +2,11 @@ class Intention < ActiveRecord::Base
   belongs_to :student
   belongs_to :user
 
+  enum current_status: [:wait_call, :contacting, :booking, :wait_pay, :signed_up]
+
   def next_contact_date
     last_contact = ContactLog.where(student_id: student.id).order(created_at: :desc).first
+    return nil if last_contact.nil?
     if last_contact.attribute_present?(:next_contact_at) then
       last_contact.next_contact_at.to_date
     else
