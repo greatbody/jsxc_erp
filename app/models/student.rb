123775f066164382b7b300dde6f12025a330353f
greatbody <wedown@163.com>
2015-12-09 15:46:32 +0800

    add model validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,9 +1,13 @@
 class Student < ActiveRecord::Base
-
-  belongs_to :user
-
   enum sex: [:female, :male, :unknown]
 
+  validates :phone, uniqueness: { message: '手机号码重复.' }, presence: { message: '请输入手机号码.'}, format: { with: /\A1\d{10}\z/, message: '手机号码格式不正确.' }
+  validates :name, presence: { message: '请输入姓名' }
+  validates :id_card, format: { with: /\A\d{17}(\d|(x|X))\z/, message: '身份证号码格式不正确.' }, allow_nil: true, on: :update
+  validates :address, length: { maximum: 255, message: '地址超出长度限制' }
+  validates :unit, length: { maximum: 255, message: '单位名称超出长度限制' }
+
+  belongs_to :user
   has_one :intention
   has_many :contact_logs
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :rememberable, :trackable
 
   validates :phone, uniqueness: { message: '手机号码重复.' }, presence: { message: '请输入手机号码.'}, format: { with: /\A1\d{10}\z/, message: '手机号码格式不正确.' }
-  validates :name, presence: true
+  validates :name, presence: { message: '请输入姓名' }
   has_many :students
   has_many :intentions
   has_many :contact_logs
