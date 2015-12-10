b9f36d0a8f103c28e3bd7891856523a7f6df299b
greatbody <wedown@163.com>
2015-12-10 13:47:45 +0800

    delete files

--- app/models/intention.rb
+++ app/models/intention.rb
@@ -1,5 +1,5 @@
 class Intention < ActiveRecord::Base
-  enum current_status: [:wait_call, :contacting, :booking, :wait_pay, :signed_up]
+  enum current_status: [:wait_call, :contacting, :booking, :wait_pay, :signed_up, :canceled]
 
   belongs_to :student
   belongs_to :user
