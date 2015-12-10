bad292d482fbcb626bf5caac2c994635b7008d66
greatbody <wedown@163.com>
2015-12-10 14:12:32 +0800

    tests

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe StudentController, type: :controller do
+RSpec.describe StudentsController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
--- spec/models/contact_log_spec.rb
+++ spec/models/contact_log_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe ContactLog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  pending "ad22d some examples to (or delete) #{__FILE__}"
 end
