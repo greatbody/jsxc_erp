a0f9b2c48e88726327bb332058d86f6b77ce07f9
greatbody <wedown@163.com>
2015-12-10 15:02:59 +0800

    change count display

--- spec/models/contact_log_spec.rb
+++ spec/models/contact_log_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe ContactLog, type: :model do
-  pending "ad22d some examples to (or delete) #{__FILE__}"
+  pending "add some examples to (or delete) #{__FILE__}"
 end
