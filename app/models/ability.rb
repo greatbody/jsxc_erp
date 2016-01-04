7df3fa66a966c21b6f826f792725e59187711ba5
greatbody <wedown@163.com>
2016-01-04 16:09:07 +0800

    ability setting

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,16 +1,18 @@
 class Ability
   include CanCan::Ability
   def initialize(user)
-    if user && user.admin?
-      if user.role? :superadmin
-        can :manage, :all             # allow superadmins to do anything
-      elsif user.role? :manager
-        can :manage, [User, Product]  # allow managers to do anything to products and users
-      elsif user.role? :sales
-        can :update, Product, :hidden => false  # allow sales to only update visible products
-      end
-    else
-      can :dashboard
+    if user.has_role? :superadmin
+      can :manage, :all             # allow superadmins to do anything
+    elsif user.has_role? :user
+      can :manage, [Intention, ContactLog, ResidenceCard, Student]  # allow managers to do anything to products and users
+      can [:assign_task, :search], :service
+    elsif user.has_role? :intern
+      # can :cru, [Intention, ContactLog, ResidenceCard, Student]
+      # can :get_intention_list_api, [Intention]
+      can [:create, :read, :update, :get_intention_list], [Intention]
+      can [:create, :read], [ContactLog]
+      can [:create, :read], [Student]
+      can [:search], :service
     end
   end
 end
