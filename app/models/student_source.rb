b69af2b64c26939f5f5bed80ef341d702ed8ab9c
greatbody <wedown@163.com>
2016-01-05 20:14:40 +0800

    add enum for student_sources

--- app/models/student_source.rb
+++ app/models/student_source.rb
@@ -4,4 +4,12 @@ class StudentSource < ActiveRecord::Base
   has_many :source_contacts
   has_many :intentions
   has_many :source_contracts
+
+  def self.student_source_for_select
+    student_source_collection = [['[空]', '']]
+    StudentSource.all.each do |student_source|
+      student_source_collection << [student_source.name, student_source.id]
+    end
+    student_source_collection
+  end
 end
