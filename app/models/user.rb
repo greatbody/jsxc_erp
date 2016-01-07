0a068c0a7f976249e9c4f9e59b223badf4a2ad7c
greatbody <wedown@163.com>
2016-01-07 10:32:05 +0800

    add relationship user has many student_sources

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ActiveRecord::Base
   has_many :students
   has_many :intentions
   has_many :contact_logs
+  has_many :student_sources
 
   def before_add_method(role)
     # do something before it gets added
