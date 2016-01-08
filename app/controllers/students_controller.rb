3416ff9a65352b6b96c6a07ac507c5697587a0fe
greatbody <wedown@163.com>
2016-01-08 19:59:20 +0800

    增加一个学员信息列表

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,6 +3,7 @@ class StudentsController < ApplicationController
   before_action :set_student, only: [:show, :edit, :update, :get_student_status, :notify_got_number, :notify_got_card]
   load_and_authorize_resource
   def index
+    @students = Student.all
   end
 
   def new
