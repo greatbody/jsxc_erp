022335bdf4060761e5db6caa13234e43b692b414
greatbody <wedown@163.com>
2016-01-11 09:08:43 +0800

    签到bug修复

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,14 +5,14 @@ class Ability
       can :manage, :all             # allow superadmins to do anything
     elsif user.has_role? :user
       can :manage, [Intention, ContactLog, ResidenceCard, Student, StudentSource, SourceContact]  # allow managers to do anything to products and users
-      can [:assign_task, :search], :service
+      can [:assign_task, :search, :daily_sign], :service
     elsif user.has_role? :intern
       # can :cru, [Intention, ContactLog, ResidenceCard, Student]
       # can :get_intention_list_api, [Intention]
       can [:create, :read, :update, :get_intention_list], [Intention]
       can [:create, :read], [ContactLog]
       can [:create, :read], [Student]
-      can [:search], :service
+      can [:search, :daily_sign], :service
     end
   end
 end
