eec5cb40655706e11b578037b269dd920a7c804a
greatbody <wedown@163.com>
2016-01-14 00:06:23 +0800

    新增建议报名数量显示

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,15 @@ class WelcomeController < ApplicationController
   require 'phone_ext'
   before_action :authenticate_user!, only: [:index]
   def index
+    start = Date.today.ago(1.month).to_date
+    finish = Date.today
+    @dates = []
+    @datas = []
+    while(start < finish) do
+      start += 1.day
+      @dates << start.strftime("%m-%d")
+      @datas << Student.where(signed_at: start).count
+    end
   end
 
   def demo
