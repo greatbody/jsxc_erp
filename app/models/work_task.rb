934b2cdca888b1de133730b00ddc4bd8bea9f980
greatbody <wedown@163.com>
2016-02-25 18:10:05 +0800

    model修改，增加了from_user,to_user的支持

--- app/models/work_task.rb
+++ app/models/work_task.rb
@@ -1,2 +1,17 @@
 class WorkTask < ActiveRecord::Base
+  def from_user
+    if User.where(id: from_user_id).count > 0
+      User.find(from_user_id)
+    else
+      nil
+    end
+  end
+
+  def to_user
+    if User.where(id: to_user_id).count > 0
+      User.find(to_user_id)
+    else
+      nil
+    end
+  end
 end
