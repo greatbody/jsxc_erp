c277bba5e4cbd241e23617527e59f4a9960dd5c0
greatbody <wedown@163.com>
2016-02-29 14:20:01 +0800

    增加月度、季度信息

--- app/controllers/open_controller.rb
+++ app/controllers/open_controller.rb
@@ -17,7 +17,25 @@ class OpenController < ApplicationController
       GROUP BY `student_sources`.`id`, `student_sources`.`name`
       ORDER BY count(*) DESC
     EOS
+
+    month_sql = <<-EOS
+      SELECT `student_sources`.`id` AS id, `student_sources`.`name` AS name, count(`students`.`id`) AS count FROM `student_sources`
+      LEFT OUTER JOIN `intentions` ON `intentions`.`student_source_id` = `student_sources`.`id`
+      LEFT OUTER JOIN `students` ON `students`.`id` = `intentions`.`student_id` AND `students`.`signed_at` BETWEEN DATE_ADD(curdate(),INTERVAL -30 DAY) AND curdate()
+      GROUP BY `student_sources`.`id`, `student_sources`.`name`
+      ORDER BY count(*) DESC
+    EOS
+
+    season_sql = <<-EOS
+      SELECT `student_sources`.`id` AS id, `student_sources`.`name` AS name, count(`students`.`id`) AS count FROM `student_sources`
+      LEFT OUTER JOIN `intentions` ON `intentions`.`student_source_id` = `student_sources`.`id`
+      LEFT OUTER JOIN `students` ON `students`.`id` = `intentions`.`student_id` AND `students`.`signed_at` BETWEEN DATE_ADD(curdate(),INTERVAL -90 DAY) AND curdate()
+      GROUP BY `student_sources`.`id`, `student_sources`.`name`
+      ORDER BY count(*) DESC
+    EOS
     @rank_week = StudentSource.connection.select_all(week_sql)
+    @rank_month = StudentSource.connection.select_all(month_sql)
+    @rank_season = StudentSource.connection.select_all(season_sql)
     render layout: 'work_tasks'
   end
 end
