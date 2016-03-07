eef11569e0ae07fb79702cb9c7a912386b321b27
greatbody <wedown@163.com>
2016-03-07 17:27:28 +0800

    增加，接受线上预约试学通知

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -7,10 +7,46 @@ class ApiController < ApplicationController
   def notify
     if Authorize.verify?(params[:api])
       action_params = JSON.parse(params[:api][:content]).deep_symbolize_keys
-      send_html(action_params.to_json)
-      render html: action_params.to_json
+      result = divide_task(action_params)
+      render json: result
     else
       render html: 'error'
     end
+  end
+
+  private
+
+  def divide_task(content)
+    if content[:job].present?
+      case content[:job]
+      when 'appointment'
+        appointment(content[:data])
+      end
+    end
+  end
+
+  def appointment(data)
+    student = Student.new(student_params(data))
+    intention = Intention.new(intention_params(data))
+    intention.student = student
+    if student.save && intention.save
+      { error_code: '0', error_message: '' }
+    else
+      { error_code: '3', error_message: 'update error' }
+    end
+  end
+
+  def intention_params(data)
+    {
+      source: '线上系统预约',
+      current_status: 2,  # 预约试学
+      next_contact_at: data[:appointment_date]
+    }
+  end
+
+  def student_params(data)
+    {
+      phone: data[:phone], name: data[:name]
+    }
   end
 end
