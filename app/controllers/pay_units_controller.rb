cc65de4733840f9812eee46f0e93ce4b7661cf26
greatbody <wedown@163.com>
2016-11-07 23:18:41 +0800

    new feature:费用报表中加入‘未归类’记录标签

--- app/controllers/pay_units_controller.rb
+++ app/controllers/pay_units_controller.rb
@@ -4,10 +4,16 @@ class PayUnitsController < ApplicationController
 
   def payments
     @pay_unit_id = params[:pay_unit_id]
-    @pay_unit = PayUnit.find(params[:pay_unit_id])
-    @payments = @pay_unit.all_payments.order(done_at: :desc)
-    @total_paid = @pay_unit.pay_record.sum(:amount)
-    @total_get = @pay_unit.get_record.sum(:amount)
+    if @pay_unit_id.to_i == -1
+      @payments = Payment.where("get_unit_id is null or pay_unit_id is null").order(done_at: :desc)
+      @total_paid = 0
+      @total_get = 0
+    else
+      pay_unit = PayUnit.find(params[:pay_unit_id])
+      @payments = pay_unit.all_payments.order(done_at: :desc)
+      @total_paid = pay_unit.pay_record.sum(:amount)
+      @total_get = pay_unit.get_record.sum(:amount)
+    end
     render :partial => 'payments'
   end
 end
